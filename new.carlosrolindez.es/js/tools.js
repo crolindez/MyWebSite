1cfab0cec22efb8dbc41c43cb86d35a7db70bcb9
Carlos <crolindez@gmail.com>
2018-02-06 21:58:25 +0100

    First commit

---
+++ new.carlosrolindez.es/js/tools.js
@@ -0,0 +1,61 @@
+$(function () {
+	$("#first_exp_div").click(function() {
+		
+		$("#first_exp_div").addClass("activated");
+		$("#first_exp_txt_div").addClass("activated");
+		$("#second_exp_div").removeClass("activated");
+		$("#second_exp_txt_div").removeClass("activated");
+	
+	});
+	
+	$("#second_exp_div").click(function() {
+		$("#first_exp_div").removeClass("activated");
+		$("#first_exp_txt_div").removeClass("activated");
+		$("#second_exp_div").addClass("activated");
+		$("#second_exp_txt_div").addClass("activated");
+	});
+	
+
+	$(document).on('click', 'a[href^="#"]', function (event) {
+	    event.preventDefault();
+
+	    $('html, body').animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 1000);
+	});
+
+	$("#first_exp_div").css("cursor","pointer");
+	$("#second_exp_div").css("cursor","pointer");
+	
+	$(window).scroll(function() {
+		var pos = $(window).scrollTop();
+		var barHeight = $("div.linkbar").height();
+		if( pos > (610-barHeight)) {
+			$("div.linkbar").css("opacity","0.0");
+		} else if (pos > (610-2*barHeight)) {
+			$("div.linkbar").css("opacity",((610-barHeight)-pos)/barHeight);
+		} else {		
+			$("div.linkbar").css("opacity","1.0");
+		}
+	});
+	
+});
+
+
+
+//This is a pen based off of Codewoofy's eyes follow mouse. It is just cleaned up, face removed, and then made to be a little more cartoony. https://codepen.io/Codewoofy/pen/VeBJEP
+
+/*
+
+document.getElementById("follower_area").addEventListener('mousemove', follow);
+
+var eye = document.getElementById("followed");
+
+function follow (event) {
+  var x = (eye.offsetLeft) + (eye.offsetWidth / 2);
+  var y = (eye.offsetTop) + (eye.offsetHeight / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.style.transform='rotate(' + rot + 'deg)'
+};
+*/
